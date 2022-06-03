bc8609fa6c3edddc83a1befb1afba35eb6d5f906
Joshua Ji <joshuanianji@users.noreply.github.com>
2022-06-03 16:45:36 -0600

    Refactor Python (#22)
    * Refactor Python
    
    * Add comments
    
    * Bug fix: `home` -> `index`
    
    * Remove type annotations
    
    So carrie's computer can work

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 import os
+from typing import Any
 from flask import Flask, make_response, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 
-
+# Base content all pages need
+# used by the "profile" section of the template
 base_content = {
     'name': 'MLH Fellow',
     'position': 'Software Engineer',
@@ -18,23 +20,16 @@ base_content = {
         'name': 'LinkedIn',
         'url': 'https://www.linkedin.com/company/major-league-hacking/',
         'icon': './static/img/social/linkedin.svg'
-    }],
-    # Whether it is an initial page load, and we should play the page load animations
-    'initial': True,
+    }]
 }
 
-# TODO: Refactor these out
 
 @app.route('/')
 def index():
-    content = {
-        **base_content,
-        'title': 'Home - Portfolio',
-        'active_tab': 'home'
-    }
-    resp = make_response(render_template('index.html', **content))
-    resp.set_cookie('prev_page', 'home')
-    return resp
+    # no extra content needed
+    # title and active_tab handled by `handle_route`
+    return handle_route('Home', 'index', base_content)
+
 
 @app.route('/about')
 def about():
@@ -42,72 +37,30 @@ def about():
         **base_content,
         'quote': 'Only one who devotes himself to a cause with his whole strength and soul can be a true master. For this reason mastery demands all of a person.',
         'author': 'Albert Einstein',
-        'title': 'About - Portfolio',
-        'active_tab': 'about'
     }
-    # check prev_page cookie to see what animations we have to do
-    prev_page = request.cookies.get('prev_page', None, type=str)
-    print(f'prev_page for about: {prev_page}')
-    if prev_page is not None and prev_page != 'about':
-        # Here, we have a prev_page (so the user clicked on the navbar) and it wasn't the about page
-        # thus, there is a sliding page transition we need to do
-        content = {
-            **content,
-            'initial': False,
-            'content_slide_animation': get_animation(prev_page, 'about')
-        }
-    # set the prev_page cookie to "about", so the next link will know what page transition to do
-    resp = make_response(render_template('about.html', **content))
-    resp.set_cookie('prev_page', 'about')
-    return resp
+    return handle_route('About', 'about', content)
 
 
 @app.route('/education')
 def education():
     content = {
         **base_content,
-        'title': 'Education - Portfolio',
-        'active_tab': 'education'
     }
-    prev_page = request.cookies.get('prev_page', None, type=str)
-    print(f'prev_page for education: {prev_page}')
-    if prev_page is not None and prev_page != 'education':
-        content = {
-            **content,
-            'initial': False,
-            'content_slide_animation': get_animation(prev_page, 'education')
-        }
-    resp = make_response(render_template('education.html', **content))
-    resp.set_cookie('prev_page', 'education')
-    return resp 
+    return handle_route('Education', 'education', content)
 
 
 @app.route('/hobbies')
 def hobbies():
     content = {
         **base_content,
-        'title': 'Hobbies - Portfolio',
-        'active_tab': 'hobbies'
     }
-    prev_page = request.cookies.get('prev_page', None, type=str)
-    print(f'prev_page for hobbies: {prev_page}')
-    if prev_page is not None and prev_page != 'hobbies':
-        content = {
-            **content,
-            'initial': False,
-            'content_slide_animation': get_animation(prev_page, 'hobbies')
-        }
-    resp = make_response(render_template('hobbies.html', **content))
-    resp.set_cookie('prev_page', 'hobbies')
-    return resp
+    return handle_route('Hobbies', 'hobbies', content)
 
 
 @app.route('/where-am-i')
 def where_am_i():
     content = {
         **base_content,
-        'title': 'Where am I - Portfolio',
-        'active_tab': 'where-am-i',
         'places': [{
             'name': 'San Francisco',
             'description': 'I am currently living in San Francisco, California (lie)',
@@ -118,23 +71,51 @@ def where_am_i():
             'coords': [53, -113]
         }]
     }
+    return handle_route('Where am I', 'where-am-i', content)
+
+
+def handle_route(name: str, id: str, content):
+    '''
+    Handles routing logic for each page
+
+    Args:
+        name: Page name (Shows in the navbar)
+        id: unique id for the page
+        content: extra content to pass to the template
+    '''
+    content = {
+        **content,
+        'title': f'{name} - Portfolio',
+        'active_tab': id,
+    }
+    # check prev_page cookie to see what animations we have to do
     prev_page = request.cookies.get('prev_page', None, type=str)
-    print(f'prev_page for where-am-i: {prev_page}')
-    if prev_page is not None and prev_page != 'where-am-i':
+    print(f'prev_page for {id}: {prev_page}')
+    if prev_page is None or prev_page == id:
+        # This is an initial page load (user first navigates, or refreshes)
+        # `initial` is used by the template to know to play the fadein animations
+        content = {
+            **content,
+            'initial': True,
+        }
+    else:
+        # This is not an initial page load, so set a slide animation for the content
         content = {
             **content,
             'initial': False,
-            'content_slide_animation': get_animation(prev_page, 'where-am-i')
+            'content_slide_animation': get_animation(prev_page, id)
         }
-    resp = make_response(render_template('where-am-i.html', **content))
-    resp.set_cookie('prev_page', 'where-am-i')
+    # set the prev_page cookie to the `id`, 
+    # so the next link will know what page transition to do
+    resp = make_response(render_template(f'{id}.html', **content))
+    resp.set_cookie('prev_page', id)
     return resp
 
 
 # from the two pages, gets the animate.css animation to play
 # either a `animate__slideInLeft` or `animate__slideInRight`
 def get_animation(prev_page: str, curr_page: str) -> str:
-    pages = {'home': 0, 'about': 1, 'education': 2, 'hobbies': 3, 'where-am-i': 4}
+    pages = {'index': 0, 'about': 1, 'education': 2, 'hobbies': 3, 'where-am-i': 4}
     anim = 'slideInRight' if pages[prev_page] < pages[curr_page] else 'slideInLeft'
     return f'animate__{anim}'
 
