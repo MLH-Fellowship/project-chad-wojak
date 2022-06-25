6e40a28aaff6887e94117ec21a3fb0eb2f4c6461
CarrieKam <36577595+CarrieKam@users.noreply.github.com>
2022-06-24 20:59:17 -0400

    start the service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,4 +4,4 @@ git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-Restart=always                                                        
+systemctl start myportfolio                                                      
