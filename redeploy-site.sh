411b50a72a17b72198e7e42d77e243f17edfc21e
Kismet-Fate <yoloswagftw545@gmail.com>
2022-06-27 18:11:50 -0400

    taken out the first time setup things from redeplo-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,9 +2,5 @@ cd project-chad-wojak
 git fetch && git reset origin/main --hard
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
-python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
-systemctl start myportfolio
-systemctl enable myportfolio
-systemctl daemon-reload
 systemctl restart myportfolio
